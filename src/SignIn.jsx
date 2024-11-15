b8eae75cce973d11fbd87309ac296c693351c28c
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-11-15 17:52:12 +0200

    exercise b 10.8 complete

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,7 +1,41 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import InputField from "./components/InputField";
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  return (
+    <View style={styles.container}>
+      <InputField placeholder="Username" />
+      <InputField placeholder="Password" />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    gap: 15,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 4,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 4,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
 
 export default SignIn;
