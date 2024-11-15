b8bd30b5f59bd63c14822847626fd457186de293
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-11-15 17:22:24 +0200

    exercise b 10.7 complete

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,29 +1,31 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import AppBarTab from "./AppBarTab";
 import { Link } from "react-router-native";
+
+const AppBar = () => {
+  const tabs = [
+    { title: "Repositories", path: "/" },
+    { title: "Sign In", path: "/sign-in" },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal>
+        {tabs.map(({ title, path }) => (
+          <Link key={title} to={path} style={{ paddingRight: 15 }}>
+            <AppBarTab title={title} />
+          </Link>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     padding: Constants.statusBarHeight,
     backgroundColor: "#24292e",
-    flexDirection: "row",
-    gap: 25,
   },
-  // ...
 });
-
-const AppBar = () => {
-  return (
-    <View style={styles.container}>
-      <Link to={"/"}>
-        <AppBarTab title="Repositories" />
-      </Link>
-      <Link to={"/sign-in"}>
-        <AppBarTab title="Sign In" />
-      </Link>
-    </View>
-  );
-};
-
 export default AppBar;
