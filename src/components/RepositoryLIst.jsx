1723b2b1f18001bde5a984251199452744e2f922
sulochan barakoti <sulochanbarakoti@gmail.com>
2025-03-08 17:47:00 +0200

    Update expo dependency and implement custom hook for fetching repositories

--- src/components/RepositoryLIst.jsx
+++ src/components/RepositoryLIst.jsx
@@ -1,5 +1,6 @@
 import { FlatList, View, StyleSheet } from "react-native";
 import RepositoryItem from "./RepositoryItem";
+import useRepositories from "../hooks/useRepositories";
 
 const styles = StyleSheet.create({
   separator: {
@@ -57,9 +58,16 @@ const repositories = [
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
+  const { repositories } = useRepositories();
+
+  // Get the nodes from the edges array
+  const repositoryNodes = repositories
+    ? repositories.edges.map((edge) => edge.node)
+    : [];
+
   return (
     <FlatList
-      data={repositories}
+      data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) => <RepositoryItem repository={item} />}
     />
